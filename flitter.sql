5baefdc19de24263ff556e849ce266e408bf825b
Nick Yonts <NickYonts@Gmail.Com>
2015-10-16 10:29:23 -0400

    Updated flitter.sql to use IDs rather than the handle.

--- flitter.sql
+++ flitter.sql
@@ -1,25 +1,27 @@
 CREATE TABLE IF NOT EXISTS Users
 (
-ID int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-Handle varchar(255) UNIQUE,
-Password varchar(255) NOT NULL,
-LastName varchar(255) NOT NULL,
-FirstName varchar(255) NOT NULL,
-Location varchar(255) NOT NULL
+ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+Handle VARCHAR(255) UNIQUE,
+Password VARCHAR(255) NOT NULL,
+LastName VARCHAR(255) NOT NULL,
+FirstName VARCHAR(255) NOT NULL,
+Location VARCHAR(255) NOT NULL
 )
 
 CREATE TABLE IF NOT EXISTS Fleets
 (
 Fleet_ID INT NOT NULL AUTO_INCREMENT,
-User_Handle varchar(255) NOT NULL,
-Fleet varchar(155),
+User_ID INT NOT NULL,
+Fleet VARCHAR(155),
 PRIMARY KEY(Fleet_ID),
-FOREIGN KEY(User_Handle) REFERENCES Users(Handle)
+FOREIGN KEY(User_ID) REFERENCES Users(ID)
 )
 
 CREATE TABLE IF NOT EXISTS Following
 (
-User_Handle varchar(255) NOT NULL REFERENCES Users(Handle),
-Following_Handle varchar(255) NOT NULL REFERENCES Users(Handle),
-PRIMARY KEY(User_Handle, Following_Handle)
+User_ID INT NOT NULL,
+Following_ID INT NOT NULL,
+FOREIGN KEY(User_ID) REFERENCES Users(ID),
+FOREIGN KEY(Following_ID) REFERENCES Users(ID),
+PRIMARY KEY(User_ID, Following_ID)
 )
